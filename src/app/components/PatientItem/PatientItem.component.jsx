492d1ff972aef69c423b348a899a15fd01abc388
riz-l <joshua.riz.layton@gmail.com>
2021-03-11 12:45:29 +0000

    MASTER: Adding PatientItem progress

--- src/app/components/PatientItem/PatientItem.component.jsx
+++ src/app/components/PatientItem/PatientItem.component.jsx
@@ -65,29 +65,6 @@ export default function PatientItem() {
                   </DetailsRow>
                 </DetailsColumn>
               </DetailsColumn>
-              {/* <DetailsColumn>
-                <DetailsItem>
-                  <DetailsHeading>Name</DetailsHeading>
-                  <Details>John Smith</Details>
-                </DetailsItem>
-
-                <DetailsItem>
-                  <DetailsHeading>Age</DetailsHeading>
-                  <Details>34 years</Details>
-                </DetailsItem>
-              </DetailsColumn>
-
-              <DetailsColumn>
-                <DetailsItem>
-                  <DetailsHeading>Condition</DetailsHeading>
-                  <Details>A00 - Cardiac arrest</Details>
-                </DetailsItem>
-
-                <DetailsItem>
-                  <DetailsHeading>NEWS</DetailsHeading>
-                  <Details>2</Details>
-                </DetailsItem>
-              </DetailsColumn> */}
             </DetailsWrapper>
           </DetailsContainer>
         </Wrapper>
